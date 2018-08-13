939c22496e1e7ba58e46f60713fc7c1fd6065184
Vinod Gupta <vinod@playstudios.asia>
2018-08-13 17:28:36 +0800

    Indentation

--- SimpleInjector/SimpleInjectorSample/Program.cs
+++ SimpleInjector/SimpleInjectorSample/Program.cs
@@ -9,14 +9,19 @@ namespace SimpleInjectorSample
 
             var container = ObjectFactory.GetContainer();
             DiContainer.Register(container);
+
             ILogger logger = ObjectFactory.GetInstance<ILogger>();
             IEmployeeProcessor employeeProcessor = ObjectFactory.GetInstance<IEmployeeProcessor>();
+
             IEmployee initial = ObjectFactory.GetInstance<IEmployee>();
             logger.Log(initial);
+
             IEmployee updated = employeeProcessor.UpdateEmployee(123, "Vinod");
             logger.Log(updated);
+
             IEmployee processed = employeeProcessor.Process();
             logger.Log(processed);
+
             Console.ReadLine();
         }
     }
